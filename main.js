beca3ddae0756ca7c4d3fdc8d4866c4abddc9031
Genesis Barrios <genesisbarriosdev@gmail.com>
2020-09-26 22:03:43 -0400

    Added animations and sound

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var mainState = {
         
         //Load the pipe sprite
         game.load.image('pipe', 'assets/pipe.png');
+        
+        //Load jump sound
+        game.load.audio('jump', 'assets/jump.wav');
     },
 
     create: function () {
@@ -24,11 +27,19 @@ var mainState = {
 
         //Add gravity to the bird to make it fall
         this.bird.body.gravity.y = 1000;
+        
+        //Move the anchor to the left and downward
+        this.bird.anchor.setTo(-0.2, 0.5);
 
         //Call the 'jump' function when the spacekey is hit
         var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
         
+        
+        //SOUNDS
+        
+        //Add the jump sound
+        this.jumpSound = game.add.audio('jump');
         
         
         //PIPES
@@ -38,6 +49,12 @@ var mainState = {
         
         //Add Pipes every 1.5 seconds
         this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+        
+        
+        //SCORES
+        
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0", {font: "30px Times New Roman", fill: "white"} );
     },
     
     addOnePipe: function (x, y) {
@@ -59,7 +76,7 @@ var mainState = {
     },
     
     addRowOfPipes: function () {
-        //Randomly pick a number between 1 and 5
+        //Randomly pick a number between 1 and 6
         //This will be the hole position
         var hole = Math.floor(Math.random() * 5) + 1;
         
@@ -69,21 +86,70 @@ var mainState = {
             if(i != hole && i != hole + 1)
                 this.addOnePipe(400, i * 60 + 10);
         }
+        
+        //Increase score by 1 each time new pipes are created
+        this.score += 1;
+        this.labelScore.text = this.score;
     },
 
     update: function(){
+        //bird animation
+        if(this.bird.angle < 20)
+            this.bird.angle += 1;
+        
         //If the bird is out of the screen(too high or low)
         //Call the 'restartGame' function
         if(this.bird.y < 0 || this.bird.y > 490)
             this.restartGame();
+        
+        //call 'restartGame' function each time bird collides with the 'pipes' group
+        game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
     },
 
     jump: function(){
+        if(this.bird.alive == false)
+            return;
+        
+        //bird animation
+        var animation = game.add.tween(this.bird);
+        
+        //change the angle of the bird to -20 in 100 milliseconds
+        animation.to({angle: -20}, 100);
+        animation.start();
+        
         //Add a vertical velocity to the bird
         this.bird.body.velocity.y = -350;
+        
+        //Play the jump sound
+        this.jumpSound.play();
     },
 
+    hitPipe: function(){
+        //If the bird has already hit a pipe, do nothing
+        //It means the bird is already falling off the screen
+        if(this.bird.alive == false)
+            return;
+        
+        //Set the alive property of the bird to false
+        this.bird.alive = false;
+        
+        //change score color to red
+        this.labelScore.addColor("#ff0000", 0);
+        
+        //Prevent new pipes from appearing
+        game.time.events.remove(this.timer);
+        
+        //Go through all the pipes, and stop their movement
+        this.pipes.forEach(function(p){
+            p.body.velocity.x = 0;
+        }, this);
+    },
+    
     restartGame: function(){
+        
+        //change score color to red
+        this.labelScore.addColor("#ff0000", 0);
+        
         //Start the 'main' state, which restarts the game
         game.state.start('main');
     }
